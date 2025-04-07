7dbd3186b23b681e72e7b886df2b771784697ef7
parashuram23007 <kotreshmanjula07@gmail.com>
2025-04-06 22:19:51 -0700

    Add files via upload

---
+++ ankitha demo.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class Demo1 {
+	public static void main(String args[]){
+		int a=50+34;
+		int b=43-23;
+		int c=a/62;
+		int d=a/5;
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		System.out.println("c="+c);
+		System.out.println("d="+d);
+		
+	}
+	
+	
+
+}
